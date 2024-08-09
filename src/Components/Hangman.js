c3b219b4498eddf9379293014cf7a61dc18182bd
Ziad-Abdallah <ziad38107@gmail.com>
2024-08-10 02:15:47 +0300

    initial commit

--- src/Components/Hangman.js
+++ src/Components/Hangman.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import hangman6 from '../hangman-game-images/images/hangman-6.svg'
-import hangman5 from '../hangman-game-images/images/hangman-5.svg'
-import hangman4 from '../hangman-game-images/images/hangman-4.svg'
-import hangman3 from '../hangman-game-images/images/hangman-3.svg'
-import hangman2 from '../hangman-game-images/images/hangman-2.svg'
-import hangman1 from '../hangman-game-images/images/hangman-1.svg'
-import hangman0 from '../hangman-game-images/images/hangman-0.svg'
+import hangman6 from '../images/hangman-6.svg'
+import hangman5 from '../images/hangman-5.svg'
+import hangman4 from '../images/hangman-4.svg'
+import hangman3 from '../images/hangman-3.svg'
+import hangman2 from '../images/hangman-2.svg'
+import hangman1 from '../images/hangman-1.svg'
+import hangman0 from '../images/hangman-0.svg'
 
 import { useSelector } from 'react-redux'
 
